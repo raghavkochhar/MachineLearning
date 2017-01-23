1bde4601dbee26cf44eac57568a092560ebefc56
sulwyn3 <sulwyn3@gmail.com>
2017-01-23 14:30:10 -0500

    NEW Neural NETS

--- neural_networks.py
+++ neural_networks.py
@@ -5,50 +5,108 @@ from sklearn.model_selection import train_test_split
 from sklearn import datasets
 from sklearn import svm
 import matplotlib.pyplot as plt
+import copy
 import matplotlib
+import time
 
 matplotlib.style.use('ggplot')
 
-
-# data = pd.read_csv("data/HR.csv")
-# y = data['left']
-
-
-iris = datasets.load_iris()
-
-#Set Train/Test Split Percentages
-train_percent = np.arange(0.05, 1, 0.05)
-test_percent = 1 - train_percent
-
-#Create arrays to store train & validation accuracies
-train_accuracy = np.zeros(len(train_percent))
-test_accuracy = np.zeros(len(train_percent))
-
-num_iterations = 10
-
-for i in range(len(test_percent)):
-
-    for j in range(num_iterations):
-        x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=test_percent[i], \
-                                                            random_state=j)
-        #Neural Nets
-        clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-        clf.fit(x_train, y_train)
-        #SVM
-        # clf = svm.SVC(kernel='linear', C=1).fit(x_train, y_train)
-        train_accuracy[i] += clf.score(x_train, y_train)
-        test_accuracy[i] += clf.score(x_test, y_test)
-
-train_accuracy = train_accuracy/num_iterations
-test_accuracy = test_accuracy/num_iterations
+#Start Timer
+start_time = time.time()
 
 
-results_df = pd.DataFrame({'Train':train_accuracy, 'Test':test_accuracy},index = train_percent)
 
-print results_df
+def HR_data():
+    # # INCLUDE DEPARTMENTS Analytics
+    data = pd.read_csv("data/HR.csv")
 
-results_df.plot()
-plt.show()
+    # Due to the data having 15k instances and being unmanageable. The data will be split into 50/50
+    # positive/negative samples to reduce the instances to 7k
+
+    data = data.iloc[np.random.permutation(len(data))]
+    data = data.sort_values(by=['left'])
+    num_pos = data['left'].sum(axis=0)
+
+    data50 = pd.concat([data.iloc[:num_pos,:], data.iloc[-num_pos:,:]])
+
+    #Create X and Y datsets
+    datay = data50['left']
+    datax = data50.copy()
+    del datax['left']
+    del datax['department']
+
+    print datax.columns
+
+    #One-Hot Encode categorical variables for scikit compantibility
+    datax = pd.get_dummies(datax, columns =['number_project','salary'], drop_first=True)
+
+
+    # # INCLUDE DEPARTMENTS
+    # del datax['left']
+    # datax = pd.get_dummies(datax, columns =['number_project','department','salary'], drop_first=True)
+
+    return datax, datay
+
+
+
+
+def learning_curve(datax, datay, num_iterations):
+
+    # Set Train/Test Split Percentages
+    train_percent = np.arange(0.05, 0.95, 0.05)
+    test_percent = 1 - train_percent
+
+    # Create arrays to store train & validation accuracies
+    train_accuracy = np.zeros(len(train_percent))
+    test_accuracy = np.zeros(len(train_percent))
+
+
+    for i in range(len(test_percent)):
+
+        for j in range(num_iterations):
+            x_train, x_test, y_train, y_test = train_test_split(datax, datay, test_size=test_percent[i], \
+                                                                random_state=j)
+
+            print "------------%s---------------" % (train_percent[i])
+            print "TRAIN COUNT: %s/ %s" %(sum(y_train), len(y_train))
+            print "TEST COUNT: %s/%s" % (sum(y_test), len(y_test))
+
+            #Neural Nets
+            #alpha is L2 penalty,
+            #
+            # clf = MLPClassifier(solver='lbfgs', alpha=1e-5, activation= 'logistic', \
+            #                     hidden_layer_sizes=(100,), random_state=j, \
+            #                     early_stopping = True).fit(x_train, y_train)
+
+            #SVM
+            # clf = svm.LinearSVC(C=1).fit(x_train, y_train)
+
+            clf = svm.SVC(C=1, kernel ='rbf', random_state=j).fit(x_train, y_train)
+            train_accuracy[i] += clf.score(x_train, y_train)
+            test_accuracy[i] += clf.score(x_test, y_test)
+
+    train_accuracy = train_accuracy/num_iterations
+    test_accuracy = test_accuracy/num_iterations
+
+
+    return pd.DataFrame({'Train':train_accuracy, 'Test':test_accuracy}, index = train_percent)
+
+
+# Run HR Analytics
+
+# datax, datay = HR_data()
+# results_df = learning_curve(datax, datay, num_iterations = 10)
+
+
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+# print results_df
+# results_df.plot()
+# plt.show()
+
 
 
 
